a0894c3fbc138c5876b522079b87e8ed437fcb8e
HAKUPA11 <ishnarsh1119@gmail.com>
2025-08-26 19:42:12 +0530

    added dentytech frontend landing page clone

---
+++ HOTEL_ODISEJ_FRONTEND_PROJECT/HOTEL_ODISEJ_FRONTEND.js
@@ -0,0 +1,16 @@
+// for only one element
+
+// var texth1=document.querySelector("#firsth1").textContent
+
+// // console.log(texth1)
+// var v= texth1.split("")
+// // console.log(v)
+// var clutter=""
+// // v.forEach(function(elem){
+// //     clutter += `<span>${elem}</span>`
+// // })
+// for(var i=0;i<v.length;i++){
+//     clutter+=`<span>${v[i]}</span>`
+// }
+
+// document.querySelector("#firsth1").innerHTML=clutter
